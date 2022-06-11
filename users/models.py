91bbe359c0c9b78f16ab598a23dd55f8d64dda92
glen15 <glen910115@gmail.com>
2022-06-11 23:09:49 +0900

    ADD: gender choices in gender filed

--- users/models.py
+++ users/models.py
@@ -6,6 +6,16 @@ class User(AbstractUser):
 
     """Custom User Model"""
 
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
     avatar = models.ImageField(null=True)
-    gender = models.CharField(max_length=10, null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
     bio = models.TextField(default="write your bio")
