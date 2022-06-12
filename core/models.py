741829faa4b6ffd35c751a855a0efed8378d6809
glen15 <glen910115@gmail.com>
2022-06-12 14:50:14 +0900

    ADD: migrtation room

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStampedModel(models.Model):
 
     """Time Stamped Model"""
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)  # 생성될때 마다 그 시간 남김
+    updated = models.DateTimeField(auto_now=True)  # 저장될때마다 그 시간 남김
 
     class Meta:
         abstract = True  # db에 안올라가고 코드상에서만 보임
